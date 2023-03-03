bb74062fe28cd0330ee6f686fcd68a10d1b5e821
Алексей <py6jlb@gmail.com>
2023-03-03 17:06:58 +0300

    feat: добавляет репозиторий для работы с callback
    #4

---
+++ src/Services/Bot/Afonya.Bot.Domain/Entities/Callback.cs
@@ -0,0 +1,12 @@
+﻿using Afonya.Bot.Domain.Enums;
+using LiteDB;
+
+namespace Afonya.Bot.Domain.Entities;
+
+public class Callback
+{
+    public ObjectId Id { get; set; }
+    public CallbackCommand Command { get; set; }
+    public Guid? GroupId { get; set; }
+    public string JsonData { get; set; }
+}
