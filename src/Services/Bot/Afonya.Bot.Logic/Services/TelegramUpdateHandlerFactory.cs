a0088d6a84af8defa4f317dd309ad3fd6eb6beaf
Алексей <py6jlb@gmail.com>
2023-03-06 09:41:40 +0300

    refactor: переносит Starter в хост проект и убеирает его зависисмость от репозиториев
    #13

--- src/Services/Bot/Afonya.Bot.Logic/Services/TelegramUpdateHandlerFactory.cs
+++ src/Services/Bot/Afonya.Bot.Logic/Services/TelegramUpdateHandlerFactory.cs
@@ -5,30 +5,29 @@ using Afonya.Bot.Logic.TelegramUpdateHandlers;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using IUpdateHandler = Afonya.Bot.Interfaces.Services.UpdateHandler.IUpdateHandler;
 
 namespace Afonya.Bot.Logic.Services;
 
-public class UpdateHandlerFactory : IUpdateHandlerFactory
+public class TelegramUpdateHandlerFactory : ITelegramUpdateHandlerFactory
 {
     private readonly ILoggerFactory _loggerFactory;
     private readonly ITelegramBotClient _botClient;
     private readonly IMoneyTransactionRepository _moneyTransaction;
-    private readonly ICategoryRepository _categoryRepository;
     private readonly IBotKeyboardService _botKeyboard;
     private readonly ICallbackRepository _callbackRepository;
 
-    public UpdateHandlerFactory(ILoggerFactory loggerFactory, ITelegramBotClient botClient, IMoneyTransactionRepository moneyTransaction, ICategoryRepository categoryRepository, IBotKeyboardService botKeyboard, ICallbackRepository callbackRepository)
+    public TelegramUpdateHandlerFactory(ILoggerFactory loggerFactory, 
+        ITelegramBotClient botClient, IMoneyTransactionRepository moneyTransaction, 
+        IBotKeyboardService botKeyboard, ICallbackRepository callbackRepository)
     {
         _loggerFactory = loggerFactory;
         _botClient = botClient;
         _moneyTransaction = moneyTransaction;
-        _categoryRepository = categoryRepository;
         _botKeyboard = botKeyboard;
         _callbackRepository = callbackRepository;
     }
 
-    public IUpdateHandler CreateHandler(Update update)
+    public ITelegramUpdateHandler CreateHandler(Update update)
     {
         return update switch
         {
---
+++ src/Services/Bot/Afonya.Bot.WebWorker/BackgroundTasks/Starter.cs
@@ -0,0 +1,65 @@
+﻿using Afonya.Bot.Logic.Commands.Bot.BotStop;
+using Afonya.Bot.Logic.Commands.Categories.AddCategory;
+using Afonya.Bot.Logic.Queries.GetCategoryCount;
+using MediatR;
+using Shared.Contracts;
+
+namespace Afonya.Bot.WebWorker.BackgroundTasks;
+
+public class Starter : IHostedService
+{
+    private readonly ILogger<Starter> _logger;
+    private readonly IConfiguration _configuration;
+
+    public Starter(
+        ILogger<Starter> logger, 
+        IServiceScopeFactory scopeFactory,
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+
+        Scope = scopeFactory.CreateScope();
+    }
+
+    private IServiceScope Scope { get; }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await InitCategories(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Завершение работы приложение, попытка отключить webhook.");
+        var mediator = Scope.ServiceProvider.GetRequiredService<IMediator>();
+        await mediator.Send(new BotStopCommand(), cancellationToken);
+        Scope.Dispose();
+
+    }
+
+    private async Task InitCategories(CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Инициализация категорий");
+        var mediator = Scope.ServiceProvider.GetRequiredService<IMediator>();
+        var count = await mediator.Send(new GetCategoriesCountQuery(), cancellationToken);
+        if (count > 0)
+        {
+            _logger.LogDebug("В БД уже есть категории, инициализация пропущена.");
+            return;
+        }
+        var categories = _configuration.GetSection("Categories").GetChildren().ToArray();
+        _logger.LogDebug("Получено {Count} категорий, из файла конфигурации.", categories.Count());
+        
+        foreach (var category in categories)
+        {
+            var categoryDto = category.Get<CategoryDto>();
+            if (categoryDto == null) continue;
+            categoryDto.IsActive = true;
+            await mediator.Send(new AddCategoryCommand
+            {
+                NewCategory = categoryDto
+            }, cancellationToken);
+        }
+    }
+}
