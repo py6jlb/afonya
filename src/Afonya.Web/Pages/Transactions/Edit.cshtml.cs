c0d125d3d6473b2958aad5b6a1557696aee8dc27
py6jlb <pyj6lb@gmail.com>
2025-06-12 17:58:09 +0300

    -иконки -редактирование

---
+++ src/Afonya.Web/Pages/Transactions/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Afonya.Web.Pages.Transactions
+{
+    public class EditTransactionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
